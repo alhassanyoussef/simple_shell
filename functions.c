290f4cca642ce6f2d8828eb554e1f5923dfec15c
alhassanyoussef <hyoussef650@.com>
2023-09-12 20:37:08 +0300

    simple_shell

---
+++ functions.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+
+/**
+ * Ha_print - Prints a string to the standard output
+ * @text: The string to be printed
+ *
+ * Return: void
+ */
+void Ha_print(const char *text)
+{
+	write(STDOUT_FILENO, text, strlen(text));
+}
+
+/**
+ * my_prompt - Prints a prompt to the standard output
+ *
+ * Return: void
+ */
+void my_prompt(void)
+{
+	Ha_print("Ha_shell$");
+}
---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+void Ha_print(const char *text);
+void my_prompt(void);
+
+#endif
